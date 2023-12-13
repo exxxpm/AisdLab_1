44c432a586c99ec81b323b7e239022889c142a2c
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-12-13 17:09:16 +0400

    The implementation of constructors and the overload operator to get a reference to an image element

--- lab 1.h
+++ lab 1.h
@@ -39,3 +39,84 @@ class Image{
 		}
 };
 
+template <typename T>
+Image<T>::Image(int x, int y, bool flag, T fillValue){
+	image = new T * [x];
+	for (int i = 0; i < x; i++){
+		image[i] = new T[y];
+	}
+
+	size_x = x;
+	size_y = y;
+	if (flag == true){
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+		for (int i = 0; i < x; i++){
+			for (int j = 0; j < y; j++){
+				image[i][j] = dis(gen);
+			}
+		}
+	}else{
+		for (int i = 0; i < x; i++){
+			for (int j = 0; j < y; j++){
+				image[i][j] = fillValue;
+			}
+		}
+	}
+}
+
+
+Image<bool>::Image(int x, int y, bool flag){
+	image = new bool* [x];
+	for (int i = 0; i < x; i++){
+		image[i] = new bool[y];
+	}
+
+	size_x = x;
+	size_y = y;
+	if (flag == true){
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				image[i][j] = rand() % 2;
+			}
+		}
+	}else{
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				image[i][j] = 0;
+			}
+		}
+	}
+}
+
+
+template <typename T>
+Image<T>::Image(int x, int y, bool flag){
+	image = new T * [x];
+	for (int i = 0; i < x; i++){
+		image[i] = new T[y];
+	}
+
+	size_x = x;
+	size_y = y;
+	if (flag == true){
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				image[i][j] = rand();
+			}
+		}
+	}else{
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				image[i][j] = 0;
+			}
+		}
+	}
+}
+
+
+template <typename T>
+T& Image<T>::operator()(int x, int y){
+	return image[x][y];
+}
