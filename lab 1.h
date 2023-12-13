23aa4ddfb62b975c4321431f4176b326efaee6e2
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-12-13 17:30:28 +0400

    Implementation of operators

--- lab 1.h
+++ lab 1.h
@@ -26,8 +26,6 @@ class Image{
 		Image& operator!();
 		float image_koefficient() const;
 
-
-
 		friend std::ostream& operator<<(std::ostream& os, Image<T>& I){
 			for (int i = 0; i < I.size_x; i++) {
 				for (int j = 0; j < I.size_y; j++) {
@@ -66,7 +64,6 @@ Image<T>::Image(int x, int y, bool flag, T fillValue){
 	}
 }
 
-
 Image<bool>::Image(int x, int y, bool flag){
 	image = new bool* [x];
 	for (int i = 0; i < x; i++){
@@ -89,7 +86,6 @@ Image<bool>::Image(int x, int y, bool flag){
 		}
 	}
 }
-
 
 template <typename T>
 Image<T>::Image(int x, int y, bool flag){
@@ -115,8 +111,152 @@ Image<T>::Image(int x, int y, bool flag){
 	}
 }
 
-
 template <typename T>
 T& Image<T>::operator()(int x, int y){
 	return image[x][y];
+}
+
+template <typename T>
+Image<T>& Image<T>::operator+(const T a) {
+	Image<T> result(size_x, size_y, false);
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			result.image[i][j] = image[i][j] + a;
+			if (result.image[i][j] < image[i][j])
+				result(i, j) = numeric_limits<T>::max();
+			else
+				result(i, j) = result.image[i][j];
+		}
+	}
+	return result;
+}
+
+
+template <typename T>
+Image<T>& Image<T>::operator*(const T a) {
+	Image<T> result(size_x, size_y, false);
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			result.image[i][j] = image[i][j] * a;
+			if (result.image[i][j] < image[i][j])
+				result(i, j) = numeric_limits<T>::max();
+			else
+				result(i, j) = result.image[i][j];
+		}
+	}
+	return result;
+}
+
+
+Image<bool>& Image<bool>::operator*(const Image<bool>& a) {
+	if (a.size_x != size_x || a.size_y != size_y) {
+		throw image_exception();
+	}
+
+	Image<bool> result(size_x, size_y, false);
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			result.image[i][j] = (bool)(image[i][j] & a.image[i][j]);
+		}
+	}
+	return result;
+}
+
+
+template <typename T>
+Image<T>& Image<T>::operator*(const Image<T>& a) {
+	if (a.size_x != size_x || a.size_y != size_y) {
+		throw image_exception();
+	}
+
+	Image<T> result(size_x, size_y, false);
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			result.image[i][j] = image[i][j] * static_cast<T>(a.image[i][j]);
+			if (result.image[i][j] < image[i][j])
+				result(i, j) = numeric_limits<T>::max();
+			else
+				result(i, j) = result.image[i][j];
+		}
+	}
+	return result;
+}
+
+
+
+Image<bool>& Image<bool>::operator+(const Image<bool>& a) {
+	if (a.size_x != size_x || a.size_y != size_y) {
+		throw image_exception();
+	}
+
+	Image<bool>* result = new Image<bool>(size_x, size_y, false);
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			result->image[i][j] = (bool)(image[i][j] | a.image[i][j]);
+		}
+	}
+	return *result;
+}
+
+
+
+template <typename T>
+Image<T>& Image<T>::operator+(const Image<T>& a) {
+	if (a.size_x != size_x || a.size_y != size_y) {
+		throw image_exception();
+	}
+
+	Image<T> result(size_x, size_y, false);
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			result.image[i][j] = image[i][j] + static_cast<T>(a.image[i][j]);
+			if (result.image[i][j] < image[i][j])
+				result(i, j) = numeric_limits<T>::max();
+			else
+				result(i, j) = result.image[i][j];
+		}
+	}
+	return result;
+}
+
+
+template <typename T>
+Image<T>& Image<T>::operator!() {
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			image[i][j] = -1 * image[i][j];
+		}
+	}
+	return *this;
+}
+
+template <typename T>
+bool Image<T>::operator==(Image<T> a){
+	if (a.size_x != size_x || a.size_y != size_y) {
+		throw image_exception();
+	}
+
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			if (a.image[i][j] != image[i][j]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool Image<float>::operator==(Image a){
+	if (a.size_x != size_x || a.size_y != size_y) {
+		throw image_exception();
+	}
+
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			if (!(a.image[i][j] - image[i][j] <= eps || image[i][j] - a.image[i][j] <= eps)) {
+				return false;
+			}
+		}
+	}
+	return true;
 }
