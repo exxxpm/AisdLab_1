cdf0e6e97bb7d0b6e6aadbd02719975362c011bf
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-12-13 16:55:34 +0400

    Creating a class structure and getting input data

--- lab 1.cpp
+++ lab 1.cpp
@@ -4,5 +4,43 @@
 using namespace std;
 
 int main(){
+    int size_x, size_y;
+    cout << "Enter the dimensions of the field" << endl;
+    cout << "a: ";
+    cin >> size_x;
+    cout << "b: ";
+    cin >> size_y;
 
+    int choice;
+    cout << "Choose an option:" << endl;
+    cout << "1. Fill with random values" << endl;
+    cout << "2. Fill with zeros" << endl;
+    cout << "3. Fill with a specific symbol" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    char fillSymbol;
+    if (choice == 3) {
+        cout << "Enter the symbol to fill the square with: ";
+        cin >> fillSymbol;
+    }
+    else {
+        fillSymbol = '0';
+    }
+
+    Image<char> a(size_x, size_y, false, fillSymbol);
+
+    double x1, y1;
+    cout << "Enter the coordinates of the top-left point" << endl;
+    cout << "x: ";
+    cin >> x1;
+    cout << "y: ";
+    cin >> y1;
+
+    double x2, y2;
+    cout << "Enter the coordinates of the bottom-right point" << endl;
+    cout << "x: ";
+    cin >> x2;
+    cout << "y: ";
+    cin >> y2;
 }
--- lab 1.h
+++ lab 1.h
@@ -8,5 +8,34 @@ struct image_exception {};
 
 template <typename T>
 class Image{
+	private:
+		int size_x;
+		int size_y;
+		T** image;
+		const double eps = 0.0001;
 
+	public:
+		Image(int x, int y, bool flag, T fillValue);
+		Image(int x, int y, bool flag);
+		T& operator()(int x, int y);
+		bool operator==(Image a);
+		Image& operator*(const Image& a);
+		Image& operator+(const Image& a);
+		Image& operator+(const T a);
+		Image& operator*(const T a);
+		Image& operator!();
+		float image_koefficient() const;
+
+
+
+		friend std::ostream& operator<<(std::ostream& os, Image<T>& I){
+			for (int i = 0; i < I.size_x; i++) {
+				for (int j = 0; j < I.size_y; j++) {
+					os << I.image[i][j] << " ";
+				}
+				os << '\n';
+			}
+			return os;
+		}
 };
+
